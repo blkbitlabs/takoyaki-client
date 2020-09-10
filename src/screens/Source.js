4a4fd876f8cd44e82e852b86c7da86b97b64c078
chetasr <me@chetasr.co>
2020-09-10 23:33:08 +0530

    Refactor source

--- src/screens/Source.js
+++ src/screens/Source.js
@@ -1,67 +1,79 @@
-import React, {useEffect, useState} from 'react';
+/* Settings Page - blkbit inc. */
+
+/* NPM Imports */
+import React from 'react';
 import {
   View,
   TouchableOpacity,
   Text,
   StyleSheet,
   Dimensions,
-} from 'react-native';
-var RNFS = require('react-native-fs');
-const {width, height} = Dimensions.get('window');
+} from "react-native";
+
+/* Variables */
+var RNFS = require("react-native-fs");
+
+/* Constants */
+const { width, height } = Dimensions.get("window");
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
-const scale = size => (width / guidelineBaseWidth) * size;
-const verticalScale = size => (height / guidelineBaseHeight) * size;
-const stylesheet_source = StyleSheet.create({
-  chapterbutton: {
+const scale = (size) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+
+/* Styles */
+const styles = StyleSheet.create({
+  source_container: {
     marginTop: scale(16),
-    alignItems: 'center',
+    alignItems: "center",
     width: width - scale(40),
     height: verticalScale(35),
     borderRadius: 9,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
-  chapterbutton_: {
+
+  source_background: {
     paddingHorizontal: 10,
     height: verticalScale(40),
-    width: '98.4%',
-    justifyContent: 'center',
-    backgroundColor: '#423F46',
+    width: "98.4%",
+    justifyContent: "center",
+    backgroundColor: "#423F46",
     borderBottomRightRadius: 7,
     borderTopRightRadius: 7,
   },
-  chapterbutton_tag: {
-    width: '1.6%',
-    height: verticalScale(40),
-    backgroundColor: '#5F5F5F',
-    borderBottomLeftRadius: 7,
-    borderTopLeftRadius: 7,
-  },
-  chapterbutton_name: {
-    fontFamily: 'SFProDisplay-Regular',
-    color: 'white',
-    fontSize: 15,
+
+  source_text: {
+    fontFamily: "SFProDisplay-Regular",
+    color: "white",
+    fontSize: 17,
   },
 });
+
+/* Main Code */
 function Source(props) {
-  function lets__see(x) {
+  /* Source Component Generator */
+
+  function store_source_id(x) {
+    /* Store source id in file */
     RNFS.writeFile(
-      RNFS.CachesDirectoryPath + '/' + 'Source.txt',
+      RNFS.CachesDirectoryPath + "/" + "Source.txt",
       `${x}`,
-      'utf8',
+      "utf8"
     );
   }
+
   return (
-    <View style={stylesheet_source.chapterbutton}>
-      <View style={stylesheet_source.chapterbutton_tag} />
+    <View style={styles.source_container}>
       <TouchableOpacity
-        style={stylesheet_source.chapterbutton_}
+        style={styles.source_background}
         onPress={() => {
-          lets__see(String(props.id_));
-        }}>
-        <Text style={stylesheet_source.chapterbutton_name}>{props.name_}</Text>
+          store_source_id(String(props.id_));
+        }}
+      >
+        <Text style={styles.source_text}>{props.name_}</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+/* Exports */
 export default Source;
