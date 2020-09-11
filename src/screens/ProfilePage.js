0122cc5d33f58682eb9847544e530e7f6cccf973
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2020-09-12 01:40:56 +0530

    outside scroll

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -337,13 +337,29 @@ function ProfilePage({ route, navigation }) {
         <Text style={styles_profilepage.subtitle}>GENRES</Text>
         <View style={styles_profilepage.tagscontainer}>{genres_button}</View>
         <Text style={[styles_profilepage.chapterscontainer]}>CHAPTERS</Text>
-        <FlatList>
-          data={jason.chapters},
-          renderItem={(item) => 
-            
-
-          }
-        </FlatList>
+        <SafeAreaView style={{flex: 1}}>
+          <FlatList>
+            data={jason.chapters}
+            renderItem={(item) => 
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('MangaReader', {
+                    id_chap: item.id,
+                    id_src: srcc,
+                  });
+                }}
+                style={styles_profilepage.chapterbutton}>
+                <View style={styles_profilepage.chapterbutton_tag} />
+                <View style={styles_profilepage.chapterbutton_}>
+                  <Text style={styles_profilepage.chapterbutton_name}>
+                    {item.title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            }
+            keyExtractor={(item) => item.id}
+          </FlatList>
+        </SafeAreaView>      
       </View>
     );
   } else {
@@ -439,9 +455,11 @@ function ProfilePage({ route, navigation }) {
               <Text style={styles_profilepage.newbuttonfont}> Download </Text></View>
             </TouchableOpacity>
           </View>
-          {Not_Title_Text}
         </View>
       </ScrollView>
+      <View >
+        {Not_Title_Text}
+      </View>
     </View>
   );
 }
